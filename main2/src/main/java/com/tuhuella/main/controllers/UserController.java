0cc0e490eba4453ceb0424b766af22b29acc54b0
Vir <74280026+virdc@users.noreply.github.com>
2021-11-02 21:09:09 -0300

    UserController
    creamos la clase UserController , get-register y pos-register

---
+++ main2/src/main/java/com/tuhuella/main/controllers/UserController.java
@@ -0,0 +1,47 @@
+package com.tuhuella.main.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.tuhuella.main.entities.Zone;
+import com.tuhuella.main.services.UserService;
+	
+
+@Controller
+@RequestMapping("/user")
+
+public class UserController {
+	@Autowired
+	private UserService userService;
+
+
+
+	@GetMapping("/register")
+	public String form() {
+		return "form-user";
+	}
+
+	@PostMapping("/register")
+	public String saveUser(ModelMap modelo, @RequestParam String name, @RequestParam String surname,
+			@RequestParam String email, @RequestParam String password,@RequestParam String userName,@RequestParam String street,@RequestParam Integer streetNumber,@RequestParam Zone zone,@RequestParam Integer phoneNumber,@RequestParam Integer alternativeNumber) {
+
+		try {
+			userService.signUpUser(null, name, surname, userName, password, null, street, streetNumber, zone, phoneNumber, alternativeNumber, email);
+
+			modelo.put("exito", "registro exitoso");
+			return "form-usuario";
+		} catch (Exception e) {
+			modelo.put("error", e.getMessage());
+			return "form-usuario";
+		}
+	}
+}
+
+
+
+

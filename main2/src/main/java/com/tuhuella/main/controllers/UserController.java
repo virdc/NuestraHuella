0ecb154834a7a7c782fa0e3314b8c8e53b7534a8
jramire5 <jjnicoramirez@gmail.com>
2021-11-04 19:58:13 -0300

    Errores arreglado
    esperemos que se hayan borrado los duplicados ↑↑

--- main2/src/main/java/com/tuhuella/main/controllers/UserController.java
+++ main2/src/main/java/com/tuhuella/main/controllers/UserController.java
@@ -16,7 +16,6 @@ import com.tuhuella.main.entities.Photo;
 import com.tuhuella.main.entities.User;
 import com.tuhuella.main.entities.Zone;
 import com.tuhuella.main.services.UserService;
-	
 
 @Controller
 @RequestMapping("/user")
@@ -25,66 +24,48 @@ public class UserController {
 	@Autowired
 	private UserService userService;
 
-
 	@GetMapping("/register")
 	public String form() {
-<<<<<<< Updated upstream
-		return "form-usuario";
-	}
-=======
+
 		return "singup-form";
 	}
-	
->>>>>>> Stashed changes
 
 	@PostMapping("/register")
-	public String saveUser(ModelMap modelo,@RequestParam Photo photo ,@RequestParam String name, @RequestParam String surname,@RequestParam Date birthDate,	@RequestParam String email, @RequestParam String password,@RequestParam String userName,@RequestParam String street,@RequestParam Integer streetNumber,@RequestParam Date date,
-	@RequestParam Integer phoneNumber,@RequestParam Integer alternativeNumber, @RequestParam Zone zone) {
+	public String saveUser(ModelMap modelo, @RequestParam Photo photo, @RequestParam String name,
+			@RequestParam String surname, @RequestParam Date birthDate, @RequestParam String email,
+			@RequestParam String password, @RequestParam String userName, @RequestParam String street,
+			@RequestParam Integer streetNumber, @RequestParam Date date, @RequestParam Integer phoneNumber,
+			@RequestParam Integer alternativeNumber, @RequestParam Zone zone) {
 
 		try {
-			userService.signUpUser(photo, name, surname, userName, password, date, street, streetNumber, zone, phoneNumber, alternativeNumber, email);
+			userService.signUpUser(photo, name, surname, userName, password, date, street, streetNumber, zone,
+					phoneNumber, alternativeNumber, email);
 
 			modelo.put("exito", "registro exitoso");
-<<<<<<< Updated upstream
-			return "form-usuario";
-		} catch (Exception e) {
-			modelo.put("error", e.getMessage());
-			return "form-usuario";
-=======
 			return "singup-form";
 		} catch (Exception e) {
 			modelo.put("error", e.getMessage());
 			return "singup-form";
->>>>>>> Stashed changes
 		}
 	}
-	
+
 	@GetMapping("/showUserList")
-	public String show (ModelMap modelo) {
+	public String show(ModelMap modelo) {
 		List<User> users = userService.findUsers();
-		modelo.addAttribute("Users",users);
+		modelo.addAttribute("Users", users);
 		return "show-users";
 	}
+
 	@GetMapping("/edit/{id}")
 	public String edit(@PathVariable String id, ModelMap modelo) {
 		User user = userService.edit(id);
 		modelo.addAttribute("User", user);
-		return  "edit-user";		
+		return "edit-user";
 	}
-<<<<<<< Updated upstream
 
-=======
 	@GetMapping("/login")
 	public String login() {
 		return "login";
 	}
->>>>>>> Stashed changes
-	
 
-
-	
 }
-
-
-
-
